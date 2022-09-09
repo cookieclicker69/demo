a2d8d260a360c81355e8b375a4cae658eb1da799
cookieclicker69 <112493821+cookieclicker69@users.noreply.github.com>
2022-09-09 14:52:05 -0500

    Update and rename index.html to header.php

---
+++ header.php
@@ -0,0 +1,28 @@
+<!-- better site design for site -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Site Demo</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+
+<nav class="navbar navbar-expand-sm bg-light">
+  <ul class="navbar-nav">
+    <li class="nav-item">
+      <a class="nav-link" href="#">Games</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Cheats</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Other</a>
+    </li>
+  </ul>
+</nav>
+<br>
